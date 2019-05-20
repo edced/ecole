b103744ba6302a8677bc7c0b557cad64e8d488b8
unknown <cedricdewaerheijd@gmail.com>
2019-05-20 10:40:02 +0200

    keydown

--- ex 0.py
+++ ex 0.py
@@ -5,6 +5,11 @@ import pygame
 pygame.init()
 
 
+def print(*a, **b):
+    import builtins, sys
+    builtins.print(*a, **b)
+    sys.stdout.flush()
+    
 
 #ecran
 taille = [700, 500]
@@ -14,7 +19,16 @@ ecran = pygame.display.set_mode(taille)
 image_perso = pygame.image.load('cessau.png').convert_alpha()
 image_perso_tournee = pygame.transform.rotozoom(image_perso, 90, 0.5)
 
-
+#touche
+while True:
+    image_perso_tournee = pygame.transform.rotozoom(image_perso, 90, 0.5)
+    for event in pygame.event.get():
+        if event.type == KEYDOWN:
+            if event.key == K_d:
+                a=a+5
+            elif event.key == K_q:
+                a=a-5
+    pygame.display.update()
 
 
 #couleur
@@ -53,7 +67,7 @@ while fini == 0:
     
     if a< 0:
         sens=1
-    if a>750:
+    if a>650:
         sens=-1
     
     
@@ -68,12 +82,12 @@ while fini == 0:
     
     # pygame.draw.polygon(ecran, ROUGE, [[0,50], [100,0], [100,100]])
     if sens == -1:
-        ecran.blit(image_perso_tournee, [a + 0, 400])
+        ecran.blit(image_perso_tournee, [a, 400])
     else:
-        ecran.blit(image_perso_tournee, [a + 0, 400])
+        ecran.blit(image_perso_tournee, [a, 400])
 
     pygame.display.flip()
         
-    clock.tick(120)
+    clock.tick(64)
         
 pygame.quit()
